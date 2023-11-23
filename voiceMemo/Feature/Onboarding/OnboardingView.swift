9275bad1b686e6a71fdaef4b1ef5324a0d25dec5
Bullets-jyr <jyr@bullets.co.kr>
2023-11-23 19:00:22 +0900

    02. 온보딩 화면 구현하기 - (2)

--- voiceMemo/Feature/Onboarding/OnboardingView.swift
+++ voiceMemo/Feature/Onboarding/OnboardingView.swift
@@ -5,14 +5,135 @@
 
 import SwiftUI
 
+// Main View 역할
 struct OnboardingView: View {
-  var body: some View {
-    Text("Onboarding")
-  }
+    @StateObject private var onboardingViewModel = OnboardingViewModel()
+    
+    var body: some View {
+        // TODO: - 화면 전환 구현 필요
+        OnboardingContentView(onboardingViewModel: onboardingViewModel)
+    }
+}
+
+// MARK: - 온보딩 컨텐츠 뷰
+private struct OnboardingContentView: View {
+    @ObservedObject private var onboardingViewModel: OnboardingViewModel
+    
+    fileprivate init(onboardingViewModel: OnboardingViewModel) {
+        self.onboardingViewModel = onboardingViewModel
+    }
+    
+    fileprivate var body: some View {
+        VStack {
+            // 온보딩 셀리스트 뷰
+            // Text("Onboarding")
+            OnboardingCellListView(onboardingViewModel: onboardingViewModel)
+            
+            Spacer()
+            
+            // 시작 버튼 뷰
+            StartBtnView()
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+}
+
+// MARK: - 온보딩 셀 리스트 뷰
+private struct OnboardingCellListView: View {
+    @ObservedObject private var onboardingViewModel: OnboardingViewModel
+    @State private var selectedIndex: Int
+    
+    fileprivate init(
+        onboardingViewModel: OnboardingViewModel,
+        selectedIndex: Int = 0
+    ) {
+        self.onboardingViewModel = onboardingViewModel
+        self.selectedIndex = selectedIndex
+    }
+    
+    fileprivate var body: some View {
+        TabView(selection: $selectedIndex) {
+            // 온보딩 셀
+            // enumerated(): Index를 가져옵니다.
+            // Array(): Array로 반환을 합니다.
+            ForEach(Array(onboardingViewModel.onboardingContents.enumerated()), id: \.element) { index, onboardingContent in
+                OnboardingCellView(onboardingContent: onboardingContent)
+                    .tag(index)
+            }
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 1.5)
+        .background(
+            selectedIndex % 2 == 0 ? Color.customSky : Color.customBackgroundGreen
+        )
+        .clipped()
+    }
+}
+
+// MARK: - 온보딩 셀 뷰
+private struct OnboardingCellView: View {
+    private var onboardingContent: OnboardingContent
+    
+    fileprivate init(onboardingContent: OnboardingContent) {
+        self.onboardingContent = onboardingContent
+    }
+    
+    fileprivate var body: some View {
+        VStack {
+            Image(onboardingContent.imageFileName)
+                .resizable()
+                .scaledToFit()
+            
+            HStack {
+                Spacer()
+                
+                VStack {
+                    Spacer()
+                        .frame(height: 46)
+                    
+                    Text(onboardingContent.title)
+                        .font(.system(size: 16, weight: .bold))
+
+                    Spacer()
+                        .frame(height: 5)
+                    
+                    Text(onboardingContent.subTitle)
+                        .font(.system(size: 16))
+                }
+                
+                Spacer()
+            }
+            .background(Color.customWhite)
+            // 나중에 shadow를 위해서 cornerRadius를 0으로 설정
+            .cornerRadius(0)
+        }
+        .shadow(radius: 10)
+    }
+}
+
+// MARK: - 시작하기 버튼 뷰
+private struct StartBtnView: View {
+    fileprivate var body: some View {
+        Button(
+            action: {},
+            label: {
+                HStack {
+                    Text("시작하기")
+                        .font(.system(size: 16, weight: .medium))
+                        .foregroundColor(.customGreen)
+                    
+                    Image("startHome")
+                        .renderingMode(.template)
+                        .foregroundColor(.customGreen)
+                }
+            }
+        )
+        .padding(.bottom, 50)
+    }
 }
 
 struct OnboardingView_Previews: PreviewProvider {
-  static var previews: some View {
-    OnboardingView()
-  }
+    static var previews: some View {
+        OnboardingView()
+    }
 }
